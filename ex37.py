0c48fcfef040f8d9153054d788ef351c1d0f39c3
zhangxb <izhangxb@163.com>
2017-09-16 15:58:35 +0800

    36-37

---
+++ ex37.py
@@ -0,0 +1,106 @@
+# coding=utf-8
+keyword = '''
+and: 与
+del: 删除变量
+from: 导包
+not: 非
+while: 循环
+as：修改导包的名字
+elif:else if
+global: 全局变量
+or: 或
+with: 异常捕捉的finally
+assert：断言
+else: 或者
+if：如果
+pass : 空语句，什么都不做
+yield: python生成器
+break: 跳出循环
+except: python 异常
+import : 导包
+print ：打印
+class：类
+exec:执行储存在字符串或文件中的Python语句
+in: 遍历
+raise: 自定义异常
+continue：跳出当前循环
+finally： try...finally...
+is ：对象判断
+return : 返回值
+def：定义函数
+for : 循环
+lambda: lambda表达式
+try: 异常捕捉
+'''
+
+data_type = '''
+True:布尔是
+False:布尔否
+None:空对象
+string:字符串
+numbers:数字
+floats:浮点数
+lists:列表
+'''
+
+escape_sequence = """
+\\:反斜杠
+\':单引号
+\":双引号
+\a:响铃
+\b：退格
+\f:换页
+\n:换行
+\r:回车
+\v:纵向制表符
+\t：横向制表符
+"""
+
+string_format = '''
+%d:带符号的十进制整数
+%i:同%d
+%o:不带符号的八进制数
+%u:不带符号的十进制数
+%x:不带符号的十六进制（小写）
+%X:不带符号的十六进制（大写）、
+%e:科学计数法表示的浮点数（小写）
+%E:科学计数法表示的浮点数（大写）
+%f:十进制浮点数（小写）
+%F:同%f
+%g:如果指数小于-4或者小于精度值和e相同，其余同f
+%G:如果指数小于-4或者小于精度值和E相同，其余同F
+%c:单字符
+%r:字符串（使用repr转换）
+%s:字符串（使用str转换）
+'''
+
+operator = '''
++
+-
+*
+** : a ** b a的B次幂
+/
+// 商向下取整
+%
+<
+>
+<=
+>=
+==
+!=
+<>: !=
+()
+[]
+{}
+@
+,
+.
+:
+=
++=
+-=
+/=
+//=
+%=
+**=
+'''
